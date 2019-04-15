806c9282e2fd51a1d376faea60a6e37ad9364cef
Aditya22694 <aditya.kulkarni.22694@gmail.com>
2019-04-15 16:43:59 +0530

    Test File

--- TestDemoJenKin/src/test/TestClass.java
+++ TestDemoJenKin/src/test/TestClass.java
@@ -1,4 +1,7 @@
 package test;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class TestClass {
 
@@ -6,6 +9,23 @@ public class TestClass {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello Jenkin");
+		System.out.println("Hello Jenkin");
+		System.out.println("Test hello");
+		
+		  String content = "This is the content to write into file\n";
+
+	        // If the file doesn't exists, create and write to it
+			// If the file exists, truncate (remove all content) and write to it
+	        try (FileWriter writer = new FileWriter("app.log");
+	             BufferedWriter bw = new BufferedWriter(writer)) {
+
+	            bw.write(content);
+
+	        } catch (IOException e) {
+	            System.err.format("IOException: %s%n", e);
+	        }
+
+	    }
+
 	}
 
-}
